083c032316878581de44a1bb3e3095932bccc870
Alfredo <freddevs0001@gmail.com>
2022-02-23 15:52:13 -0300

    versao2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -96,6 +96,30 @@ fun main() {
     }
 
 */
+
+
+    //Exercicio 3
+    //    3- Faça um programa que receba a idade de uma
+    //       pessoa e mostre na saída em qual categoria ela se encontra:
+    //    • 10-14 infantil
+    //    • 15-17 juvenil
+    //    • 18-25 adulto
+
+
+    print("DIGITE SUA IDADE: ")
+    val idade = readLine()!!.toInt()
+
+
+    when(idade){
+
+        in 10..14 -> print("sua categoria é infantil")
+        in 15..17 -> print("sua categoria é Juvenil")
+        in 18..25 -> print("sua categoria é Adulto")
+
+        else -> println("idade invalida")
+    }
+
+
 }
  
 
