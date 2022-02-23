103f504fe209ad7113a86c31187b6f003007add9
Alfredo <freddevs0001@gmail.com>
2022-02-23 15:34:07 -0300

    Aula 15

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "ErciciosAula15"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,113 @@
+fun main() {
+
+    // exercicio 1
+    /*Faça um programa que receba três
+     inteiros e diga qual deles é o maior.
+
+    print("DIGITE O PRIMEIRO NUMERO: ")
+    val num1= readLine()!!.toInt()
+    print("DIGITE O SEGUNDO NUMERO: ")
+    val num2= readLine()!!.toInt()
+    print("DIGITE O TERCEIRO NUMERO: ")
+    val num3= readLine()!!.toInt()
+
+    var local = num3
+
+    if(num1 > local) {
+        local = num1
+
+    }else if (num2 > local) {
+        local = num2
+    }
+    else{local=num3}
+
+    println("o numero maior é : $local")
+*/
+
+
+
+
+
+//Exercicio 5
+    //Faça um programa que o usuário digite um número
+    // e o programa retorne o mês referente ao valor.
+
+/*
+    println(
+        "DIGITE UM NUMERO DE 1 A 12 REFERENTE " +
+                "AO MÊS DO ANO A SER MOSTRADO:"
+    )
+    val mes = readLine()!!.toInt()
+
+    when (mes) {
+
+        1 -> println("O mês referente ao numero é janeiro")
+        2 -> println("O mês referente ao numero é Fevereiro")
+        3 -> println("O mês referente ao numero é Março")
+        4 -> println("O mês referente ao numero é Abril")
+        5 -> println("O mês referente ao numero é Maio")
+        6 -> println("O mês referente ao numero é Junho")
+        7 -> println("O mês referente ao numero é Julho")
+        8 -> println("O mês referente ao numero é Agosto")
+        9 -> println("O mês referente ao numero é Setembro")
+        10 -> println("O mês referente ao numero é Outubro")
+        11 -> println("O mês referente ao numero é Novembro")
+        12 -> println("O mês referente ao numero é Dezembro")
+
+
+        else -> println("Mês invalido")
+/*
+
+
+
+        //exercicio 2
+        Faça um programa que entre com três
+        números e coloque em ordem crescente.
+
+
+    print("DIGITE O PRIMEIRO NUMERO: ")
+    val num1= readLine()!!.toInt()
+    print("DIGITE O SEGUNDO NUMERO: ")
+    val num2= readLine()!!.toInt()
+    print("DIGITE O TERCEIRO NUMERO: ")
+    val num3 = readLine()!!.toInt()
+
+
+    if (num1<num2&&num1<num3&&num2<num3){
+        println("$num1 - $num2 - $num3")
+
+    } else if (num1<num3&&num1<num2&&num3<num2){
+        println("$num1- $num3 - $num2")}
+
+    else if (num2<num1&&num2<num3&&num1<num3){
+        println("$num2- $num1 - $num3")}
+
+    else if (num2<num3&&num2<num1&&num3<num1){
+        println("$num2- $num3 - $num1")}
+
+    else if (num3<num1&&num3<num2&&num1<num2){
+        println("$num3- $num1 - $num2")}
+
+    else{ println("$num3 - $num2 - $num1")}
+
+     */
+
+
+    }
+
+*/
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
